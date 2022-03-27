82a2ce0dc033bf8163b274064e84ed0d6017e975
Kevin Friday <kevinfriday87@gmail.com>
2022-03-27 16:21:04 -0500

    health calc code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,47 @@
-// first line of js 
-window.alert("This is an  alert! Javascript is running!")
 
-// This function starts the fight
-function fight() {
-    window.alert("The fight has begun")
-}
+ var fight= function(){
+    window.alert("Welcome to Robot Gladiators");
+};
 
-// prompts user for robot name
+// User robot setup
 
 var playerName =  window.prompt("What is your robots name?")
+var playerHealth = 100;
+var playerAttack = 10;
 
-window.alert("You players name is " + playerName)
 
 
+// Enemy robot setup
 
+var enemyName =  "Roborto"
+var enemyHealth = 10;
+var enemyAttack = 12;
+
+// This function starts the fight
+
+
+// Calculate player's new health
+playerHealth -= enemyAttack;
+
+// Log enemy player's results to the console
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining "
+);
+// Calculate enemy's new health
+enemyHealth -= playerAttack;
+
+// Log enemy health results to the console
+console.log(
+    playerName    + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
+);
+
+// check enemy's health
+if(enemyHealth <= 0){
+    window.alert(enemyName + "has died");
+}
+else{
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
 
 
 // function calls
