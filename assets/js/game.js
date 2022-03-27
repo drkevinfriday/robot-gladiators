3e69b9c78d0f37dfa01739cabf690a570696c26d
Kevin Friday <kevinfriday87@gmail.com>
2022-03-27 14:00:35 -0500

    intital game build

---
+++ assets/js/game.js
@@ -0,0 +1,20 @@
+// first line of js 
+window.alert("This is an  alert! Javascript is running!")
+
+// This function starts the fight
+function fight() {
+    window.alert("The fight has begun")
+}
+
+// prompts user for robot name
+
+var playerName =  window.prompt("What is your robots name?")
+
+window.alert("You players name is " + playerName)
+
+
+
+
+
+// function calls
+fight();
