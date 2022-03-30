419c9c013a48ee16716c9fe606563a80f05d2911
Kevin Friday <kevinfriday87@gmail.com>
2022-03-29 19:27:07 -0500

    fight each robot by looping2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-// var playerName = 'Clank McKrank';
+// var playerName = 'Clank McKrank' change;
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
